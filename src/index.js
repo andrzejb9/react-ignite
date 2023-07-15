83e5ba2413b6b45525e7f41e14d2538037a33d78
andrzejb9 <a.bankowski9@o2.pl>
2023-07-15 15:54:09 +0200

    5. Fetching Our First Data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+//REDUX SETUP
+import { configureStore, applyMiddleware, compose } from "@reduxjs/toolkit";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+const composeEnchancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = configureStore(
+  { reducer: rootReducer },
+  composeEnchancer(applyMiddleware(thunk))
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
+  // </React.StrictMode>
 );
