22db88e6bcf04cb1048eab341a5ab2458566cd4c
andrzejb9 <a.bankowski9@o2.pl>
2023-07-15 17:50:52 +0200

    7. Refactoring To Home

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+//Redux
+import { useDispatch } from "react-redux";
+import { loadGames } from "../actions/gamesAction";
+//Components
+import Game from "../components/Game";
+
+function Home() {
+  //FETCH GAMES
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGames());
+  });
+
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Home;
