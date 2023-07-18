013bc3a110aa1d44136c3bfe8ae06529d959f073
andrzejb9 <a.bankowski9@o2.pl>
2023-07-18 19:39:14 +0200

    21. Nav Searching

--- src/api.js
+++ src/api.js
@@ -38,7 +38,10 @@ export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${newGames}`;
 //GAME DETAILS
 export const gameDetailsURL = (game_id) =>
-  `${base_url}games/${game_id}.json?&key=${process.env.REACT_APP_API_KEY}`;
+  `${base_url}games/${game_id}.json?key=${process.env.REACT_APP_API_KEY}`;
 //Game ScreenShots
 export const gameSreenshotURL = (game_id) =>
-  `${base_url}games/${game_id}/screenshots?&key=${process.env.REACT_APP_API_KEY}`;
+  `${base_url}games/${game_id}/screenshots?key=${process.env.REACT_APP_API_KEY}`;
+//Searched game
+export const searchGameURL = (game_name) =>
+  `${base_url}games?key=${process.env.REACT_APP_API_KEY}&search=${game_name}&page_size=9`;
